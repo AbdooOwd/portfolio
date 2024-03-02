e27a04b8c45e1bad76166d589201b83ee5985214
AbdooOwd <benari102@gmail.com>
2024-03-02 13:19:17 +0100

    Added random footer text

---
+++ src/utils/abdoo/fun.ts
@@ -0,0 +1,13 @@
+export const funny_footer_text = [
+    "Hitler = Chad",
+    "Algeria's economy's boomi'",
+    "Being a millionaire? Nah, my fate's in Allah's hand",
+    "Why would pigs fly?",
+    "2/3 of my repos are useless",
+    "'Non-Romans!' said the Romans being invaded by Non-Romans.",
+    "I loved Python, now I love TypeScript",
+    "Friday Night Funkin inspired me GameDev",
+    "@NonsenseNH and @danidev inspired me coding",
+    "I should be studying right now...",
+    "Fun Fact: I know Arabic, English, French and a bit of Japanese"
+]
